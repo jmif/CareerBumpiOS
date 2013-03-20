080bfe6ced9b332a724d484f09f5a40344ccf20c
Joe Mifsud <jamifsud@umich.edu>
2013-03-20 15:34:14 -0400

    Adding git ignore and swipe to pull up bump page

---
+++ CareerBump/CBFadeSegue.m
@@ -0,0 +1,26 @@
+//
+//  CBFadeSegue.m
+//  CareerBump
+//
+//  Created by Joseph Mifsud on 3/20/13.
+//  Copyright (c) 2013 Joseph Mifsud. All rights reserved.
+//
+
+#import "CBFadeSegue.h"
+
+#import <QuartzCore/QuartzCore.h>
+
+@implementation CBFadeSegue
+
+- (void)perform
+{
+    CATransition* transition = [CATransition animation];
+    
+    transition.duration = 0.2;
+    transition.type = kCATransitionFade;
+    
+    [[self.sourceViewController navigationController].view.layer addAnimation:transition forKey:kCATransition];
+    [[self.sourceViewController navigationController] pushViewController:[self destinationViewController] animated:NO];
+}
+
+@end
---
+++ CareerBump/CBReceiveResumeViewController.m
@@ -0,0 +1,38 @@
+//
+//  CBReceiveResumeViewController.m
+//  CareerBump
+//
+//  Created by Joseph Mifsud on 3/20/13.
+//  Copyright (c) 2013 Joseph Mifsud. All rights reserved.
+//
+
+#import "CBReceiveResumeViewController.h"
+
+@interface CBReceiveResumeViewController ()
+
+@end
+
+@implementation CBReceiveResumeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
